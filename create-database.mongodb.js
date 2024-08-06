7fa8e83cd438e79e63c4dc5e56f7e6eeb68ad186
guilhermesantos4701 <guilhermesantos4701@gmail.com>
2024-08-06 20:44:23 -0300

    Criação do Banco de Dados e Coleção

---
+++ create-database.mongodb.js
@@ -0,0 +1,11 @@
+/* NOME DO BANCO DE DADOS */
+const database = 'BD3-GUILHERMEJESUS-AULA';
+
+/* NOME DO BANCO DA COLEÇÃO DE DADOS */
+const collection = 'LIVRARIA';
+
+/* CRIAR OU ACESSAR BANCO DE DADOS: */
+use(database);
+
+/* CRIAR UMA COLEÇÃO DE DAODS: */
+db.createCollection(collection)
